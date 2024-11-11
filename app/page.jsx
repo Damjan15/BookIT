472ba4969bc692735ae0347c37681246c2abef10
Damjan15 <stamenovdamjan5@gmail.com>
2024-11-11 08:55:22 +0100

    feat: RoomCard component created, rendering rooms on homepage

--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,11 @@
+import RoomCard from "@/components/RoomCard";
 import rooms from "@/data/rooms.json";
 
 export default function Home() {
   return (
     <div>
       {rooms.length > 0 ? (
-        rooms.map((room) => <h3 key={room.$id}>{room.name}</h3>)
+        rooms.map((room) => <RoomCard key={room.$id} room={room} />)
       ) : (
         <p>No rooms available at the moment.</p>
       )}
