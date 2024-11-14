b6529726b292d7404c0550bc6f14f6d7bcc78217
Damjan15 <stamenovdamjan5@gmail.com>
2024-11-14 10:36:45 +0100

    feat: fetching bookings from bookings page

--- app/bookings/page.jsx
+++ app/bookings/page.jsx
@@ -1,5 +1,16 @@
-export default function Bookings(params) {
+import Heading from "@/components/Heading";
+import getMyBookings from "@/actions/getMyBookings";
+
+export default async function Bookings(params) {
+    const bookings = await getMyBookings();
+    
     return (
-        <div>Bookings</div>
+        <div>
+            {bookings.length === 0 ? (
+                <p className="text-gray-600 mt-4">You have no bookings.</p>
+            ) : (
+                bookings.map((booking) => <h3>{booking.room_id.name}</h3>)
+            )}
+        </div>
     )
 }
