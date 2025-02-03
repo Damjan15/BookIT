d7097070ae7f6d2cfb183a4f5400244d57e67bb4
Damjan15 <stamenovdamjan5@gmail.com>
2025-02-03 09:53:26 +0100

    feat: showing rooms on homepage, rooms json file added

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,13 @@
+import rooms from "@/data/rooms.json";
+
 export default function Home() {
   return (
-    <div>
-      <h1>Hello World 👋</h1>
-    </div>
+    <>
+      {rooms.length > 0 ? (
+        rooms.map((room) => <h3>{room.name}</h3>)
+      ) : (
+        <p>No Rooms Available at the Moment</p>
+      )}
+    </>
   );
 }
