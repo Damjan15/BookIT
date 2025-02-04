963d44af05c82d57d683545880e2504b5a321de2
Damjan15 <stamenovdamjan5@gmail.com>
2025-02-04 10:34:45 +0100

    feat: login functionality added

--- actions/createSession.js
+++ actions/createSession.js
@@ -1,5 +1,8 @@
 "use server";
 
+import { cookies } from "next/headers";
+import { createAdminClient } from "@/config/appwrite";
+
 async function createSession(previousState, formData) {
   const email = formData.get("email");
   const password = formData.get("password");
@@ -12,9 +15,32 @@ async function createSession(previousState, formData) {
     };
   }
 
-  return {
-    success: true,
-  };
+  //   Get account instance
+  const { account } = await createAdminClient();
+
+  try {
+    // Generate a session
+    const session = await account.createEmailPasswordSession(email, password);
+
+    // Create cookie
+    (await cookies()).set("appwrite-session", session.secret, {
+      httpOnly: true,
+      secure: true,
+      sameSite: "strict",
+      expires: new Date(session.expire),
+      path: "/",
+    });
+
+    return {
+      success: true,
+    };
+  } catch (error) {
+    console.log("Authentication Error:", error);
+
+    return {
+      error: "Invalid Credentials",
+    };
+  }
 }
 
 export default createSession;
