01f54a597a004fa0da39c0fcdb0cee2ccec8ce3b
Damjan15 <stamenovdamjan5@gmail.com>
2025-02-05 09:15:39 +0100

    feat: register user functionality added

--- app/register/page.jsx
+++ app/register/page.jsx
@@ -1,10 +1,31 @@
+"use client";
+
+import { useEffect, useActionState } from "react";
+import { useRouter } from "next/navigation";
 import Link from "next/link";
 
+import createUser from "@/actions/createUser";
+
+import { toast } from "react-toastify";
+
 export default function Register() {
+  const [state, formAction] = useActionState(createUser, {});
+  const router = useRouter();
+
+  useEffect(() => {
+    if (state.error) {
+      toast.error(state.error);
+    }
+
+    if (state.success) {
+      toast.success("You can now log in!");
+      router.push("/login");
+    }
+  }, [state]);
   return (
     <div className="flex items-center justify-center">
       <div className="bg-white shadow-lg rounded-lg p-6 w-full max-w-sm mt-20">
-        <form>
+        <form action={formAction}>
           <h2 className="text-2xl font-bold text-center text-gray-800 mb-6">
             Register
           </h2>
